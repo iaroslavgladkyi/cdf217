6ae8fa206a9bbdf19b1ab363090b7eac52110265
iexcept <iexcept@gmail.com>
2017-03-11 12:06:48 +0200

    Add 'abstract' demo on Cats\Dogs :)

---
+++ src/week04/oop/am_abstract/Main.java
@@ -0,0 +1,14 @@
+package week04.oop.am_abstract;
+
+/**
+ * Created by codefire on 11.03.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //Pet pet = new Cat(); // LSP
+        Pet[] mypets = new Pet[] {new Cat(), new Cat(), new Dog()};
+        for (Pet pet: mypets) {
+            pet.weakUp();
+        }
+    }
+}
